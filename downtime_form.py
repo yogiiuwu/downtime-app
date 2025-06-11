6b839f07d1357281529375648019a8ad8677cba9
YOGI PRADIMI <108585340+yogiiuwu@users.noreply.github.com>
2025-06-11 20:41:10 +0700

    Update downtime_form.py

--- downtime_form.py
+++ downtime_form.py
@@ -47,11 +47,12 @@ if not st.session_state.logged_in:
 # GOOGLE SHEETS SECTION
 def get_google_sheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds_dict = dict(st.secrets)
+    creds_dict = json.loads(st.secrets.to_json())
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     spreadsheet = client.open(sheet_name)
     return spreadsheet
+
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
