e778b0c7182d6e25f16d9d7c3d42552e60e16345
YOGI PRADIMI <108585340+yogiiuwu@users.noreply.github.com>
2025-06-11 20:43:03 +0700

    Update downtime_form.py

--- downtime_form.py
+++ downtime_form.py
@@ -47,7 +47,10 @@ if not st.session_state.logged_in:
 # GOOGLE SHEETS SECTION
 def get_google_sheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds_dict = json.loads(st.secrets.to_json())
+
+    # Paling simpel, karena st.secrets sudah dict
+    creds_dict = dict(st.secrets)
+    
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     spreadsheet = client.open(sheet_name)
